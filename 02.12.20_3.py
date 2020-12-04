fab9099356ad6ee9193bbfe8559d59ca87fc6339
bolshayamedveditca <bolshayamedveditca@gmail.com>
2020-12-05 00:16:48 +0500

    файлы с домашней работой к первому уроку Пайтон

---
+++ 02.12.20_3.py
@@ -0,0 +1,6 @@
+number = input("пожалуйста,введите число\n")
+summ = float(number * 3) + float(number * 2) + float(number)
+print(summ)
+
+
+
---
+++ 02.12.20_4.py
@@ -0,0 +1,9 @@
+number = int(input("пожалуйста,введите целое число число\n"))
+max = 0
+while number > 0:
+    box = number%10
+    number = number//10
+    if max < box:
+        max = box
+
+print(f'наибольшая цифра в числе - {max}')
